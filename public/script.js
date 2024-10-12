2deffdc3ebe3021565ab2a0b92116293e2d71e60
Isac-1507 <I.Evans2010@proton.me>
2024-10-12 15:36:47 +0100

    Add pull button.

--- public/script.js
+++ public/script.js
@@ -1,36 +1,40 @@
 const delay = ms => new Promise(res => setTimeout(res, ms));
-document
-  .getElementById("guestBookForm")
-  .addEventListener("submit", function (e) {
-    e.preventDefault();
 
-    const name = document.getElementById("name").value;
-    const message = document.getElementById("message").value;
-    const status = document.getElementById("status");
+document.getElementById("guestBookForm").addEventListener("submit", function (e) {
+  e.preventDefault();
 
+  const name = document.getElementById("name").value;
+  const message = document.getElementById("message").value;
+  const status = document.getElementById("status");
 
-    fetch("/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, message }),
+  fetch("/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, message }),
+  })
+    .then((response) => response.json())
+    .then(async (data) => {
+      if (data.success) {
+        status.innerHTML += "Thank you for submitting your message!";
+        loadEntries();
+        await delay(3000);
+        status.innerHTML = "";
+      } else {
+        status.innerHTML += "There was an error submitting your message.";
+      }
     })
-      .then((response) => response.json())
-      .then(async (data) => {
-        if (data.success) {
-          status.innerHTML += "Thank you for submitting your message!";
-          loadEntries();
-          await delay(3000);
-          status.innerHTML = "";
-        } else {
-          alert("There was an error submitting your message.");
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  });
+    .catch((error) => {
+      console.error("Error:", error);
+      status.innerHTML += "An error occurred.";
+    });
+});
+
+document.getElementById('pullButton').addEventListener('click', function(e) {
+  e.preventDefault();
+  loadEntries();
+});
 
 function loadEntries() {
   fetch("/entries")
@@ -43,6 +47,9 @@ function loadEntries() {
         entryElement.innerHTML = `<strong>${entry.name}</strong><p>${entry.message}</p><hr>`;
         entriesContainer.appendChild(entryElement);
       });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
     });
 }
 
